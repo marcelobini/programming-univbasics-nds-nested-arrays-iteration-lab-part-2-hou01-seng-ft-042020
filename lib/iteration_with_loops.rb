a6e9f1979637225cac83a86fd0f5832704cf21d7
Marcelo Bini <marcelobini@gmail.com>
2020-04-03 16:54:21 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,22 +1,17 @@
-def find_min_in_nested_arrays(src)
+#def find_min_in_nested_arrays(src)
   # src will be an array of arrays of integers
   # Produce a new Array that contains the smallest number of each of the nested arrays
-  outer_results = []
-row_index = 0
-while row_index < spice_rack.count do
-  element_index = 0
-  while element_index < spice_rack[row_index].count do
-    # How to read the following line of code:
-    #   Array at row_index
-    #   Element of the inner array at element_index
-    #   The first character of that element...
-    if spice_rack[row_index][element_index][0] == "P"
-      outer_results << spice_rack[row_index][element_index]
-    end
-    element_index += 1
-  end
-  row_index += 1
-end
 
-
-end
+#end
+def find_greater_pair
+row = 0
+   array = []
+   while row < src.length do
+     if src[row][0] > src[row][1]
+       array.push(src[row][0])
+     else
+       array.push(src[row][1])
+     end
+       row += 1
+   end
+   array
