5b31af9b64522bc10e09db4b3b5b58cd77f7776f
Marcelo Bini <marcelobini@gmail.com>
2020-04-03 17:49:06 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -10,5 +10,29 @@ def find_min_in_nested_arrays(src)
     [30, 36, 38, 39, 40, 45, 47, 49, 50, 59, 60, 60, 65, 63, 61, 56, 49, 43, 40, 39, 35, 32, 23, 22],
   ]
 array_of_daily_temperatures
-  # Produce a new Array that contains the smallest number of each of the nested arrays
+outer_results = []
+row_index = 0
+while row_index < spice_rack.count do
+  element_index = 0
+  longest_string_element = ""
+  while element_index < spice_rack[row_index].count do
+
+    # Array at row_index
+    # Element of the inner array at element_index
+    # If the length of the current element is greater than the length of longest_string_element
+    # Set longest_string_element to the current element
+    if spice_rack[row_index][element_index].length > longest_string_element.length
+      longest_string_element = spice_rack[row_index][element_index]
+    end
+    element_index += 1
+  end
+
+  # We have to check every element in each inner array, so after the loop finishes
+  # The current value of longest_string_element is pushed into outer_results
+  outer_results << longest_string_element
+  row_index += 1
 end
+
+outer_results
+end
+  # Produce a new Array that contains the smallest number of each of the nested arrays
