ab78d8943108bfe3ab2948500bab92483b5a2ac4
Marcelo Bini <marcelobini@gmail.com>
2020-04-03 18:01:48 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -11,5 +11,12 @@ def find_min_in_nested_arrays(src)
   ]
 array_of_daily_temperatures
   # Produce a new Array that contains the smallest number of each of the nested arrays
-array = array.max
 end
+def find_greater_pair(src)
+   array = []
+   row_index = 0
+ while row_index < src.count do
+   array[row_index] = src[row_index].max
+   row_index += 1
+ end
+ array
